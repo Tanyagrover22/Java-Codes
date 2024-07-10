1f9028bec590f45653221cc139395a2cde04a366
Tanya Grover <162421795+Tanyagrover22@users.noreply.github.com>
2024-07-11 00:00:09 +0530

    Add files via upload

---
+++ operators/arithmetic.java
@@ -0,0 +1,17 @@
+// Java Program to implement
+// Arithmetic Operators
+import java.io.*;
+public class arithmetic {
+public static void main(String[] args) {
+     // Arithmetic operators
+     int a = 10;
+     int b = 3;
+   
+     System.out.println("a + b = " + (a + b));
+     System.out.println("a - b = " + (a - b));
+     System.out.println("a * b = " + (a * b));
+     System.out.println("a / b = " + (a / b));
+     System.out.println("a % b = " + (a % b));
+}
+    
+}
